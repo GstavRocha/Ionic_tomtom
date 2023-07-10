c2c3718301fac6262c8ac328f44df023871b9597
GustavoRocha <rocha.gustavo@escolar.ifrn.edu.br>
2023-07-10 19:37:13 -0300

    reset

--- EstudoApiTomtom/src/service/service.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-@Injectable({
-  providedIn: 'root'
-})
-export class ServiceService {
-
-  constructor(private http: HttpClient) { }
-}
